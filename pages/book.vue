2b2e732cd75b2d8d01ee4e6bf2f8ad5da59ee2c3
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-03-25 11:22:14 +0900

    検索画面から本を追加する機能実装＆編集画面作成

--- pages/book.vue
+++ pages/book.vue
@@ -1,27 +1,65 @@
 <template>
   <div>
     <!-- bookフォルダの内容が入る（このファイル名と対象のフォルダ名が同じ必要がある） -->
-    <nuxt-child @add-book-list="addBook" />
-    {{ books }}
+    <nuxt-child :books="books" @add-book-list="addBook" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import {
+  onMounted,
+  defineComponent,
+  ref,
+  useRouter,
+} from '@nuxtjs/composition-api'
 
 export default defineComponent({
   name: 'BookPage',
   setup() {
+    // 渡されてきた本の型
     type Book = {
       title: String
       image: String
       description: String
     }
-    const books = ref([] as Array<Book>)
-    
-    const addBook = (e: Book) => {
-      books.value.push(e)
+    // ローカルストレージに保存する本の型
+    type Books = {
+      id: number
+      title: String
+      image: String
+      description: String
+      readDate: ''
+      memo: ''
     }
+    const STORAGE_KEY = 'books'
+    const books = ref([] as Array<Books>)
+
+    onMounted(() => {
+      if (localStorage.getItem(STORAGE_KEY)) {
+        const localStoragebooks = localStorage.getItem(STORAGE_KEY)
+        books.value = JSON.parse(String(localStoragebooks))
+      }
+    })
+
+    const router = useRouter()
+    // ローカルストレージに保存する本の配列
+    const addBook = (e: Book) => {
+      books.value.push({
+        id: books.value.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: '',
+        memo: '',
+      })
+
+      const parsed = JSON.stringify(books.value)
+      localStorage.setItem(STORAGE_KEY, parsed)
+      // 追加した本のID
+      const id = books.value.length - 1
+      router.push(`edit/${id}`)
+    }
+
     return { books, addBook }
   },
 })
--- pages/book/edit/_id.vue
+++ pages/book/edit/_id.vue
@@ -1,15 +1,99 @@
 <template>
   <div>
-      book/edit/_id <br>
-    {{$route.params.id }}
+    <v-row class="mt-5">
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="propbook.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ propbook.title }}</v-card-title>
+
+              読んだ日：
+              <v-menu
+                v-model="menu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="date"
+                    label="Picker without buttons"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  @input="menu = false"
+                ></v-date-picker>
+              </v-menu>
+
+              メモ：
+              <v-textarea v-model="propbook.memo" class="mx-2">
+                {{ propbook.memo }}
+              </v-textarea>
+              <v-card-actions>
+                <v-btn color="secondary" to="/book"> 一覧に戻る</v-btn>
+                <v-btn color="info">保存する</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  onMounted,
+  ref,
+  useRoute,
+} from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  setup() {},
+  props: {
+    books: { type: Array, default: () => {} },
+  },
+  setup(props) {
+    const route = useRoute()
+    // Vuetifyカレンダーの設定
+    const date = ref(
+      new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10)
+    )
+    const menu = ref(false)
+
+    // ストレージから本の情報を取り出す
+    const STORAGE_KEY = 'books'
+    // 追加した本のID
+    const id = route.value.params.id
+
+    const propbook = ref('')
+    onMounted(() => {
+      // ローカルストレージに存在していれば取り出す
+      if (localStorage.getItem(STORAGE_KEY)) {
+        propbook.value = JSON.parse(String(localStorage.getItem(STORAGE_KEY)))[
+          id
+        ]
+      } else {
+        propbook.value = props.books[id]
+        const parsed = JSON.stringify(propbook.value)
+        localStorage.setItem(STORAGE_KEY, parsed)
+      }
+    })
+
+    return { propbook, date, menu }
+  },
 })
 </script>
 
